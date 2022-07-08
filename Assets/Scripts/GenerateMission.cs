8aebaf2d5f05dd1fa3ab2a831fb41ad5bcb231c3
viettung <viettungvuong@gmail.com>
2022-07-08 23:35:28 +0700

    Sua settings

--- Assets/Scripts/GenerateMission.cs
+++ Assets/Scripts/GenerateMission.cs
@@ -7,8 +7,8 @@ public class GenerateMission : MonoBehaviour
     List<Vector3> places = new List<Vector3>(5);
     Vector3[] placeVector = { new Vector3(-1835.4f,0,341.6f),
         new Vector3(-1767.3f,0,287.9f),
-        new Vector3(),
-        new Vector3()
+        new Vector3(-1650.2f,0,287.9f),
+        new Vector3(-1553.6f,0,237.5f),
     };
     class Place
     {
